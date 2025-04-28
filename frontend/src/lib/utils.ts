cd7ad169d64e8bc2e3165c0bd605741c23f9d7ca
kanza <kanza.shahid@teo-intl.com>
2025-04-28 14:34:52 +0500

    [Frontend] -backend url updated

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -5,4 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
+export const apiBaseUrl =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
